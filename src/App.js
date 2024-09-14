b335c5e1a8b54182cf5b5f0a88c48f6851a405c8
root <root@ip-172-31-95-213.ec2.internal>
2024-09-14 11:53:01 +0000

    all-to-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_API_URL}/products`)
+            .then(response => {
+                setProducts(response.data);
+            })
+            .catch(error => {
+                console.error("There was an error fetching the products!", error);
+            });
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Products</h1>
+            <ul>
+                {products.map(product => (
+                    <li key={product.id}>{product.name} - ${product.price}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
+
